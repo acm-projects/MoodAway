4968945290d08c68c33c0e1afe6d70ce6de0f40d
Nivk0 <nivedh.koya@gmail.com>
2022-10-15 19:53:55 -0500

    New commit

--- ios/MoodAway/AppDelegate.mm
+++ ios/MoodAway/AppDelegate.mm
@@ -5,7 +5,7 @@
 #import <React/RCTRootView.h>
 #import <React/RCTLinkingManager.h>
 #import <React/RCTConvert.h>
-#import <Firebase.h>
+
 #import <React/RCTAppSetupUtils.h>
 
 #if RCT_NEW_ARCH_ENABLED
